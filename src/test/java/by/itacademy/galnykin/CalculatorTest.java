f3bdc3ccfd4c1da7499175163b2fc7859e0287ab
Sergey Galnykin <galnykin@gmail.com>
2025-07-31 20:31:32 +0300

    add CalculatorTest @BeforeEach with createCalculator method

--- src/test/java/by/itacademy/galnykin/CalculatorTest.java
+++ src/test/java/by/itacademy/galnykin/CalculatorTest.java
@@ -1,4 +1,13 @@
 package by.itacademy.galnykin;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void createCalculator() {
+        calculator = new Calculator();
+    }
 }
