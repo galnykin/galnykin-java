c7a38ddce4e519252242a84b2b307124d0dc7368
Sergey Galnykin <galnykin@gmail.com>
2025-07-31 21:00:31 +0300

    add sum_TwoPositiveNumbers_ReturnsCorrectSum test method

--- src/test/java/by/itacademy/galnykin/CalculatorTest.java
+++ src/test/java/by/itacademy/galnykin/CalculatorTest.java
@@ -1,6 +1,9 @@
 package by.itacademy.galnykin;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
@@ -9,5 +12,21 @@ public class CalculatorTest {
     @BeforeEach
     public void createCalculator() {
         calculator = new Calculator();
+    }
+
+    // Sum tests
+    @Test
+    @DisplayName("Sum of two positive numbers should return correct result")
+    void sum_TwoPositiveNumbers_ReturnsCorrectSum() {
+        // Arrange
+        int a = 2;
+        int b = 3;
+        int expected = 5;
+
+        // Act
+        int result = calculator.sum(a, b);
+
+        // Assert
+        Assertions.assertEquals(expected, result);
     }
 }
