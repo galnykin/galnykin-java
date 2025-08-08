a17cabfb9fc6e72e3bb978d6b3bebdaaef2eb0e3
Sergey Galnykin <galnykin@gmail.com>
2025-08-08 11:46:40 +0300

    add dodopizza/LoginPageLocator.java

---
+++ src/main/java/by/dodopizza/LoginPageLocator.java
@@ -0,0 +1,14 @@
+package by.dodopizza;
+
+public class LoginPageLocator {
+    public static final String H3_PROVIDE_A_PHONE =
+            "//div[@class='popup-inner sc-17puf9f-0 dyohjW']//h3[contains(text(), 'Укажите телефон')]";
+    public static final String LABEL_PHONE_INPUT =
+            "//label[@for='phn-input']//span[contains(text(), 'Номер телефона')]";
+    public static final String INPUT_PHONE_INPUT =
+            "//input[@id='phn-input']";
+    public static final String BUTTON_LOGIN_SUBMIT_BUTTON =
+            "//button[@data-testid='login_submit_button']";
+    public static final String BUTTON_POPUP_CLOSE_BUTTON =
+            "//button[@class='popup-close-button']";
+}
