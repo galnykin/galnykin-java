808265d179a4b3b87b6094260cb5a189acba1ec1
Sergey Galnykin <galnykin@gmail.com>
2025-08-12 13:24:30 +0300

    add LoginPage.java

---
+++ src/main/java/by/pizzatempo/LoginPage.java
@@ -0,0 +1,8 @@
+package by.pizzatempo;
+
+public class LoginPage {
+    public static final String INPUT_LOGIN = "//input[@name='astroauth_login']";
+    public static final String INPUT_PASSWORD = "//input[@name='astroauth_pass']";
+    public static final String BUTTON_SUBMIT = "//input[@name='astroauth_submit']";
+    public static final String MESSAGE_PATH = "//div[@class='popupContent']";
+}
