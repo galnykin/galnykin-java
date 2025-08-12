b89f53501c74d6f8be483935acc0885b38a19cf6
Sergey Galnykin <galnykin@gmail.com>
2025-08-12 13:23:40 +0300

    refactor code

--- src/main/java/by/dodopizza/LoginPageLocator.java
+++ src/main/java/by/dodopizza/LoginPageLocator.java
@@ -3,7 +3,7 @@ package by.dodopizza;
 public class LoginPageLocator {
     public static final String H3_PROVIDE_A_PHONE =
             "//div[@class='popup-inner sc-17puf9f-0 dyohjW']//h3[contains(text(), 'Укажите телефон')]";
-    public static final String LABEL_PHONE_INPUT =
+    public static final String LABEL_PHONE_NUMBER =
             "//label[@for='phn-input']//span[contains(text(), 'Номер телефона')]";
     public static final String INPUT_PHONE_INPUT =
             "//input[@id='phn-input']";
