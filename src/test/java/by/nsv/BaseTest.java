d4f5e68a6712467074938d2b28652632efc5a7f6
Sergey Galnykin <galnykin@gmail.com>
2025-08-20 17:42:06 +0300

    add BaseTest.java

---
+++ src/test/java/by/nsv/BaseTest.java
@@ -0,0 +1,26 @@
+package by.nsv;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown()throws InterruptedException {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
