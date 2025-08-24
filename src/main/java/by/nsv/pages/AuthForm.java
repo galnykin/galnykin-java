e0203bacdb371d7035bb0786a9ca2f86e427f50a
Sergey Galnykin <galnykin@gmail.com>
2025-08-24 19:12:32 +0300

    refactor AuthForm.java

--- src/main/java/by/nsv/pages/AuthForm.java
+++ src/main/java/by/nsv/pages/AuthForm.java
@@ -1,57 +1,50 @@
 package by.nsv.pages;
 
-import by.nsv.webdriver.WebDriverSingleton;
+import by.nsv.webdriver.ElementUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class AuthForm {
 
     private final By AUTH_FORM_TITLE = By.xpath("//div[@class='form_head']/h2");
-    private final By CLOSE_AUTH_FORM_BUTTON =
+    private final By CLOSE_BUTTON =
             By.xpath("//div[contains(@class,'auth_frame')]/a[@class='close jqmClose']");
-
-    private final By USER_LOGIN_INPUT = By.cssSelector("#USER_LOGIN_POPUP");
-    private final By USER_PASSWORD_INPUT = By.id("USER_PASSWORD_POPUP");
+    private final By LOGIN_INPUT = By.cssSelector("#USER_LOGIN_POPUP");
+    private final By PASSWORD_INPUT = By.id("USER_PASSWORD_POPUP");
     private final By LOGIN_BUTTON = By.name("Login");
 
-    private final By USER_LOGIN_POPUP_ERROR_ID = By.id("USER_LOGIN_POPUP-error");
-    private final By USER_PASSWORD_POPUP_ERROR_ID = By.id("USER_PASSWORD_POPUP-error");
-    private final By ALERT_DANDER_MESSAGE = By.xpath("//div[@class='alert alert-danger']/p");
+    private final By LOGIN_ERROR_MESSAGE = By.id("USER_LOGIN_POPUP-error");
+    private final By PASSWORD_ERROR_MESSAGE = By.id("USER_PASSWORD_POPUP-error");
+    private final By ALERT_DANGER_MESSAGE = By.xpath("//div[@class='alert alert-danger']/p");
 
-    public String getAuthFormTitleText() {
-        return WebDriverSingleton.getTextFromElement(AUTH_FORM_TITLE);
+    public String getTitle() {
+        return ElementUtils.getText(AUTH_FORM_TITLE);
     }
 
-    public void clickCloseAuthFormButton() {
-        WebDriverSingleton.clickElement(CLOSE_AUTH_FORM_BUTTON);
+    public void clickCloseButton() {
+        ElementUtils.click(CLOSE_BUTTON);
     }
 
-    public String getAlertDangerMessage() {
-        return WebDriverSingleton.getTextFromElement(ALERT_DANDER_MESSAGE);
+    public void setLogin(String login) {
+        ElementUtils.clearAndSendKeys(LOGIN_INPUT, login);
     }
 
-    public void inputPassword(String password) {
-        WebElement passwordInput = WebDriverSingleton.findElement(USER_PASSWORD_INPUT);
-        passwordInput.clear();
-        passwordInput.sendKeys(password);
-    }
-
-    public void inputLogin(String login) {
-        WebElement loginInput = WebDriverSingleton.findElement(USER_LOGIN_INPUT);
-        loginInput.clear();
-        loginInput.sendKeys(login);
+    public void setPassword(String password) {
+        ElementUtils.clearAndSendKeys(PASSWORD_INPUT, password);
     }
 
     public void clickLoginButton() {
-        WebDriverSingleton.clickElement(LOGIN_BUTTON);
+        ElementUtils.click(LOGIN_BUTTON);
     }
 
-    public String getUserLoginPopupErrorMessage() {
-
-        return WebDriverSingleton.getTextFromElement(USER_LOGIN_POPUP_ERROR_ID);
+    public String getLoginErrorMessage() {
+        return ElementUtils.getText(LOGIN_ERROR_MESSAGE);
     }
 
-    public String getUserPasswordPopupErrorMessage() {
-        return WebDriverSingleton.getTextFromElement(USER_PASSWORD_POPUP_ERROR_ID);
+    public String getPasswordErrorMessage() {
+        return ElementUtils.getText(PASSWORD_ERROR_MESSAGE);
+    }
+
+    public String getAlertDangerMessage() {
+        return ElementUtils.getText(ALERT_DANGER_MESSAGE);
     }
 }
