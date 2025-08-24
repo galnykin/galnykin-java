83bc06284542d7058950259e6518995900d18f74
Sergey Galnykin <galnykin@gmail.com>
2025-08-24 19:11:14 +0300

    refactor BaseTest.java

--- src/test/java/by/nsv/BaseTest.java
+++ src/test/java/by/nsv/BaseTest.java
@@ -1,12 +1,28 @@
 package by.nsv;
 
-import by.nsv.webdriver.WebDriverSingleton;
+import by.nsv.pages.HomePage;
+import by.nsv.webdriver.DriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 public abstract class BaseTest {
 
+    protected HomePage homePage;
+
+    @BeforeEach
+    public void setUpBase() {
+        homePage = new HomePage();
+        homePage.openSite();
+        customizeSetup();
+    }
+
+    protected void customizeSetup() {
+        homePage.clickCloseGameBanner()
+                .clickAcceptAllCookiesButton();
+    }
+
     @AfterEach
-    public void closeBrowser() throws InterruptedException {
-        WebDriverSingleton.quit();
+    public void closeBrowser() {
+        DriverManager.quit();
     }
 }
