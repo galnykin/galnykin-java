a7f277e685864ed3ee90ff59cb0d5caae962bd69
Sergey Galnykin <galnykin@gmail.com>
2025-08-24 19:13:15 +0300

    refactor DriverManager.java

---
+++ src/main/java/by/nsv/webdriver/DriverManager.java
@@ -0,0 +1,34 @@
+package by.nsv.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public final class DriverManager {
+    private static WebDriver driver;
+
+    private DriverManager() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            initDriver();
+        }
+        return driver;
+    }
+
+    private static void initDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    public static void quit() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
