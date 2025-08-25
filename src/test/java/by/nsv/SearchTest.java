7ecf47fe7241c0c97031fe8a6ff33a69ec2ee37b
Sergey Galnykin <galnykin@gmail.com>
2025-08-25 13:34:31 +0300

    refactor SearchTest.java. Add test groups for positive and negative tests. Parametrise testSearchConcreteItem test

--- src/test/java/by/nsv/SearchTest.java
+++ src/test/java/by/nsv/SearchTest.java
@@ -3,7 +3,11 @@ package by.nsv;
 import by.nsv.pages.SearchPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -11,50 +15,63 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SearchTest extends BaseTest {
+class SearchTest extends BaseTest {
 
     SearchPage searchPage;
 
     @BeforeEach
-    public void setUpSearch() {
+    void setUpSearch() {
         searchPage = new SearchPage();
     }
 
-    @Test
-    @DisplayName("Поиск конкретного товара")
-    public void testSearchConcreteItem() {
-        String expectedItem = "Мобильный телефон Maxvi P100 (синий)";
-        searchPage.sendKeysSearch(expectedItem)
-                .startSearch();
+    @Nested
+    class PositiveTests {
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "Мобильный телефон Maxvi P100 (синий)",
+                "Смартфон HONOR Magic7 Pro 12GB/512GB (серый)",
+                "Робот-пылесос Roborock Q8 Max Pro RRA1TAL Q8MRP52-02 (с русской озвучкой, черный)"
+        })
+        @DisplayName("Поиск конкретного товара")
+        void testSearchConcreteItem(String expectedItem) {
+            assertEquals(
+                    expectedItem,
+                    searchPage.sendKeysSearch(expectedItem)
+                            .startSearch()
+                            .getSearchResultFirstItemTitleText(),
+                    "Найденный товар не совпадает с ожидаемым"
+            );
+        }
 
-        String actualItem = searchPage.getSearchResultFirstItemTitleText();
-        assertEquals(expectedItem, actualItem, "Найденный товар не совпадает с ожидаемым");
-    }
+        @Test
+        @Tag("smoke")
+        @DisplayName("Поиск товаров по ключевому слову 'телефон'")
+        void testSearchByKeyword() {
+            String keyword = "телефон";
+            searchPage.sendKeysSearch(keyword)
+                    .startSearch();
 
-    @Test
-    @DisplayName("Поиск товаров по ключевому слову 'телефон'")
-    public void testSearchByKeyword() {
-        String keyword = "телефон";
-        searchPage.sendKeysSearch(keyword)
-                .startSearch();
+            List<String> titles = searchPage.getSearchResultItemsTitleText();
 
-        List<String> titles = searchPage.getSearchResultItemsTitleText();
-
-        assertFalse(titles.isEmpty(), "Результаты поиска пустые");
-        for (String title : titles) {
-            assertTrue(title.toLowerCase().contains(keyword),
-                    "В названии товара нет ключевого слова: " + title);
+            assertFalse(titles.isEmpty(), "Результаты поиска пустые");
+            for (String title : titles) {
+                assertTrue(title.toLowerCase().contains(keyword),
+                        "В названии товара нет ключевого слова: " + title);
+            }
         }
     }
 
-    @Test
-    @DisplayName("Поиск несуществующего товара")
-    public void testSearchNothingFound() {
-        String searchQuery = "ытадлцощжш4окдаьвь";
-        searchPage.sendKeysSearch(searchQuery)
-                .startSearch();
+    @Nested
+    class NegativeTests {
+        @Test
+        @DisplayName("Поиск несуществующего товара")
+        void testSearchNothingFound() {
+            String searchQuery = "ытадлцощжш4окдаьвь";
+            searchPage.sendKeysSearch(searchQuery)
+                    .startSearch();
 
-        List<String> titles = searchPage.getSearchResultItemsTitleText();
-        assertTrue(titles.isEmpty(), "Ожидалось, что результаты будут пустыми");
+            List<String> titles = searchPage.getSearchResultItemsTitleText();
+            assertTrue(titles.isEmpty(), "Ожидалось, что результаты будут пустыми");
+        }
     }
 }
